681a4989476a3064d35a494df0bcbdc65cad4d21
DiegoGarcia271004 <00088023@uca.edu.sv>
2025-06-18 21:57:57 -0600

    ADD: Se quitan imports inutilizados

--- app/src/androidTest/java/com/example/testeableapp/TipCalculatorScreenTest.kt
+++ app/src/androidTest/java/com/example/testeableapp/TipCalculatorScreenTest.kt
@@ -1,15 +1,12 @@
 package com.example.testeableapp
 
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Slider
-import androidx.compose.ui.semantics.SemanticsActions
+
 import androidx.compose.ui.semantics.SemanticsProperties
 import androidx.compose.ui.semantics.getOrNull
 import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsOn
 import androidx.compose.ui.test.assertTextContains
-import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.isToggleable
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
@@ -18,11 +15,9 @@ import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performSemanticsAction
 import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.performTouchInput
-import androidx.compose.ui.test.swipe
 import androidx.compose.ui.test.swipeRight
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.testeableapp.ui.Screens.TipCalculatorScreen
-import com.example.testeableapp.ui.Screens.calculateTip
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
